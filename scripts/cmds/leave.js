3d68007b8f2f729c87444601cb7784f5c5b9bb3b
ariyanjim7080-lang <ariyanjim7080@gmail.com>
2025-09-09 02:14:11 +0600

    Add files via upload

--- scripts/cmds/leave.js
+++ scripts/cmds/leave.js
@@ -1,161 +1,98 @@
-const axios = require("axios");
-const fs = require("fs-extra");
-const request = require("request");
+const { getTime, drive } = global.utils;
 
 module.exports = {
-  config: {
-    name: "leave",
-    aliases: ["l"],
-    version: "2.0", 
-    author: "Vex_Kshitiz",
-    countDown: 5,
-    role: 2,
-    shortDescription: "Bot will leave a group chat",
-    longDescription: "",
-    category: "admin",
-    guide: {
-      en: "{p}{n}",
-    },
-  },
+	config: {
+		name: "leave",
+		version: "1.4",
+		author: "NTKhang",
+		category: "events"
+	},
 
-  onStart: async function ({ api, event }) {
-    try {
-      const groupList = await api.getThreadList(300, null, ['INBOX']); 
+	langs: {
+		vi: {
+			session1: "sáng",
+			session2: "trưa",
+			session3: "chiều",
+			session4: "tối",
+			leaveType1: "tự rời",
+			leaveType2: "bị kick",
+			defaultLeaveMessage: "{userName} đã {type} khỏi nhóm"
+		},
+		en: {
+			session1: "morning",
+			session2: "noon",
+			session3: "afternoon",
+			session4: "evening",
+			leaveType1: "left",
+			leaveType2: "was kicked from",
+			defaultLeaveMessage: "{userName} {type} the group"
+		}
+	},
 
-      const filteredList = groupList.filter(group => group.threadName !== null);
+	onStart: async ({ threadsData, message, event, api, usersData, getLang }) => {
+		if (event.logMessageType == "log:unsubscribe")
+			return async function () {
+				const { threadID } = event;
+				const threadData = await threadsData.get(threadID);
+				if (!threadData.settings.sendLeaveMessage)
+					return;
+				const { leftParticipantFbId } = event.logMessageData;
+				if (leftParticipantFbId == api.getCurrentUserID())
+					return;
+				const hours = getTime("HH");
 
-      if (filteredList.length === 0) {
-        api.sendMessage('No group chats found.', event.threadID);
-      } else {
-        const formattedList = filteredList.map((group, index) =>
-          `│${index + 1}. ${group.threadName}\n│𝐓𝐈𝐃: ${group.threadID}`
-        );
+				const threadName = threadData.threadName;
+				const userName = await usersData.getName(leftParticipantFbId);
 
-       
-        const start = 0;
-        const currentList = formattedList.slice(start, start + 5);
+				// {userName}   : name of the user who left the group
+				// {type}       : type of the message (leave)
+				// {boxName}    : name of the box
+				// {threadName} : name of the box
+				// {time}       : time
+				// {session}    : session
 
-        const message = `╭─╮\n│𝐋𝐢𝐬𝐭 𝐨𝐟 𝐠𝐫𝐨𝐮𝐩 𝐜𝐡𝐚𝐭𝐬:\n${currentList.join("\n")}\n╰───────────ꔪ`;
+				let { leaveMessage = getLang("defaultLeaveMessage") } = threadData.data;
+				const form = {
+					mentions: leaveMessage.match(/\{userNameTag\}/g) ? [{
+						tag: userName,
+						id: leftParticipantFbId
+					}] : null
+				};
 
-        const sentMessage = await api.sendMessage(message, event.threadID);
-        global.GoatBot.onReply.set(sentMessage.messageID, {
-          commandName: 'leave',
-          messageID: sentMessage.messageID,
-          author: event.senderID,
-          start,
-        });
-      }
-    } catch (error) {
-      console.error("Error listing group chats", error);
-    }
-  },
+				leaveMessage = leaveMessage
+					.replace(/\{userName\}|\{userNameTag\}/g, userName)
+					.replace(/\{type\}/g, leftParticipantFbId == event.author ? getLang("leaveType1") : getLang("leaveType2"))
+					.replace(/\{threadName\}|\{boxName\}/g, threadName)
+					.replace(/\{time\}/g, hours)
+					.replace(/\{session\}/g, hours <= 10 ?
+						getLang("session1") :
+						hours <= 12 ?
+							getLang("session2") :
+							hours <= 18 ?
+								getLang("session3") :
+								getLang("session4")
+					);
 
-  onReply: async function ({ api, event, Reply, args }) {
-    const { author, commandName, start } = Reply;
+				form.body = leaveMessage;
 
-    if (event.senderID !== author) {
-      return;
-    }
+				if (leaveMessage.includes("{userNameTag}")) {
+					form.mentions = [{
+						id: leftParticipantFbId,
+						tag: userName
+					}];
+				}
 
-    const userInput = args.join(" ").trim().toLowerCase();
-
-    if (userInput === 'next') {
-  
-      const nextPageStart = start + 5;
-      const nextPageEnd = nextPageStart + 5;
-
-      try {
-        const groupList = await api.getThreadList(300, null, ['INBOX']);
-        const filteredList = groupList.filter(group => group.threadName !== null);
-
-        if (nextPageStart >= filteredList.length) {
-          api.sendMessage('End of list reached.', event.threadID, event.messageID);
-          return;
-        }
-
-        const currentList = filteredList.slice(nextPageStart, nextPageEnd).map((group, index) =>
-          `${nextPageStart + index + 1}. ${group.threadName}\n𝐓𝐈𝐃: ${group.threadID}`
-        );
-
-        const message = `╭─╮\n│𝐋𝐢𝐬𝐭 𝐨𝐟 𝐠𝐫𝐨𝐮𝐩 𝐜𝐡𝐚𝐭𝐬:\n${currentList.join("\n")}\n╰───────────ꔪ`;
-
-        const sentMessage = await api.sendMessage(message, event.threadID);
-        global.GoatBot.onReply.set(sentMessage.messageID, {
-          commandName: 'leave',
-          messageID: sentMessage.messageID,
-          author: event.senderID,
-          start: nextPageStart,
-        });
-
-      } catch (error) {
-        console.error("Error listing group chats", error);
-        api.sendMessage('An error occurred while listing group chats.', event.threadID, event.messageID);
-      }
-
-    } else if (userInput === 'previous') {
-  
-      const prevPageStart = Math.max(start - 5, 0);
-      const prevPageEnd = prevPageStart + 5;
-
-      try {
-        const groupList = await api.getThreadList(300, null, ['INBOX']);
-        const filteredList = groupList.filter(group => group.threadName !== null);
-
-        if (prevPageStart < 0) {
-          api.sendMessage('Already at the beginning of the list.', event.threadID, event.messageID);
-          return;
-        }
-
-        const currentList = filteredList.slice(prevPageStart, prevPageEnd).map((group, index) =>
-          `${prevPageStart + index + 1}. ${group.threadName}\n𝐓𝐈𝐃: ${group.threadID}`
-        );
-
-        const message = `╭─╮\n│𝐋𝐢𝐬𝐭 𝐨𝐟 𝐠𝐫𝐨𝐮𝐩 𝐜𝐡𝐚𝐭𝐬:\n${currentList.join("\n")}\n╰───────────ꔪ`;
-
-        const sentMessage = await api.sendMessage(message, event.threadID);
-        global.GoatBot.onReply.set(sentMessage.messageID, {
-          commandName: 'leave',
-          messageID: sentMessage.messageID,
-          author: event.senderID,
-          start: prevPageStart,
-        });
-
-      } catch (error) {
-        console.error("Error listing group chats", error);
-        api.sendMessage('An error occurred while listing group chats.', event.threadID, event.messageID);
-      }
-
-    } else if (!isNaN(userInput)) {
-  
-      const groupIndex = parseInt(userInput, 10);
-
-      try {
-        const groupList = await api.getThreadList(300, null, ['INBOX']);
-        const filteredList = groupList.filter(group => group.threadName !== null);
-
-        if (groupIndex <= 0 || groupIndex > filteredList.length) {
-          api.sendMessage('Invalid group number.\nPlease choose a number within the range.', event.threadID, event.messageID);
-          return;
-        }
-
-        const selectedGroup = filteredList[groupIndex - 1];
-        const groupID = selectedGroup.threadID;
-
-        const botUserId = api.getCurrentUserID();
-        await api.removeUserFromGroup(botUserId, groupID);
-
-        api.sendMessage(`Left the group chat: ${selectedGroup.threadName}`, event.threadID, event.messageID);
-
-      } catch (error) {
-        console.error("Error leaving group chat", error);
-        api.sendMessage('An error occurred while leaving the group chat.\nPlease try again later.', event.threadID, event.messageID);
-      }
-
-    } else {
-      api.sendMessage('Invalid input.\nPlease provide a valid number or reply with "next" or "previous".', event.threadID, event.messageID);
-    }
-
-   
-    global.GoatBot.onReply.delete(event.messageID);
-  },
+				if (threadData.data.leaveAttachment) {
+					const files = threadData.data.leaveAttachment;
+					const attachments = files.reduce((acc, file) => {
+						acc.push(drive.getFile(file, "stream"));
+						return acc;
+					}, []);
+					form.attachment = (await Promise.allSettled(attachments))
+						.filter(({ status }) => status == "fulfilled")
+						.map(({ value }) => value);
+				}
+				message.send(form);
+			};
+	}
 };
